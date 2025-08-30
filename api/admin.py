f32b01dc31612bd234fa06fe9171b1eed94b6c8f
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-08-31 02:33:18 +0800

    Added  Group admin with profile inline

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,35 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group
+from .models import CustomUser, GroupProfile
 
-# Register your models here.
+# Custom User admin
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Institute Info", {"fields": ("institute_id",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Institute Info", {"fields": ("institute_id",)}),
+    )
+
+    list_display = ("username", "email", "institute_id", "is_staff", "is_active")
+    search_fields = ("username", "email", "institute_id")
+
+# Group admin with profile inline
+class GroupProfileInline(admin.StackedInline):
+    model = GroupProfile
+    can_delete = False
+    verbose_name_plural = "Profile"
+
+admin.site.unregister(Group)
+
+@admin.register(Group)
+class CustomGroupAdmin(GroupAdmin):
+    inlines = (GroupProfileInline,)
+    list_display = ("name", "get_system_id")
+
+    def get_system_id(self, obj):
+        return obj.profile.system_id if hasattr(obj, "profile") else "-"
+    get_system_id.short_description = "System ID"
