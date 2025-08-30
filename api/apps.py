68557322c41d477813291d86cdc6abf1a879783d
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-08-31 02:33:50 +0800

    Added iniital api app

--- api/apps.py
+++ api/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+
+# accounts/apps.py
+from django.apps import AppConfig
+
+class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
+
+    def ready(self):
+        import api.signals
