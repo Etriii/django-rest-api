77bc95eb126020717d611b0d2d12b2a9b50ca266
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-08-31 02:35:04 +0800

    chore:

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,1 @@
 from rest_framework import serializers
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'  # This will serialize all fields of the Book model
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+# from .views import BookViewSet
 
-router = DefaultRouter()
-router.register(r'books', BookViewSet)  # This creates a 'books' endpoint for CRUD operations
+# router = DefaultRouter()
+# router.register(r'books', BookViewSet)  # This creates a 'books' endpoint for CRUD operations
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+# urlpatterns = [
+#     path('api/', include(router.urls)),
+# ]
 
 
