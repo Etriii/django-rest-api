bc910ce65c8376d5e64da575b264ff03e17e7852
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-09-02 22:02:44 +0800

    Modified admin file

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import Group
-from .models import User, GroupProfile
+from .models import User #GroupProfile
 
 # Custom User admin
 @admin.register(User)
@@ -17,19 +17,19 @@ class UserAdmin(UserAdmin):
     list_display = ("username", "email", "institute_id", "is_staff", "is_active")
     search_fields = ("username", "email", "institute_id")
 
-# Group admin with profile inline
-class GroupProfileInline(admin.StackedInline):
-    model = GroupProfile
-    can_delete = False
-    verbose_name_plural = "Profile"
+# # Group admin with profile inline
+# class GroupProfileInline(admin.StackedInline):
+#     model = GroupProfile
+#     can_delete = False
+#     verbose_name_plural = "Profile"
 
-admin.site.unregister(Group)
+# admin.site.unregister(Group)
 
-@admin.register(Group)
-class CustomGroupAdmin(GroupAdmin):
-    inlines = (GroupProfileInline,)
-    list_display = ("name", "get_system_id")
+# @admin.register(Group)
+# class CustomGroupAdmin(GroupAdmin):
+#     inlines = (GroupProfileInline,)
+#     list_display = ("name", "get_system_id")
 
-    def get_system_id(self, obj):
-        return obj.profile.system_id if hasattr(obj, "profile") else "-"
-    get_system_id.short_description = "System ID"
+#     def get_system_id(self, obj):
+#         return obj.profile.system_id if hasattr(obj, "profile") else "-"
+#     get_system_id.short_description = "System ID"
