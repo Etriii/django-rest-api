ab05333f50d96c32d9b51f589171597744e7b92a
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-09-03 23:56:45 +0800

    revision: changed token route to support groups and permissions

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,28 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.views.AuthViewSet import AuthViewSet
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["username"] = user.username
+        token["permissions"] = list(user.get_all_permissions())
+        token["groups"] = list(user.groups.values_list("name", flat=True))
+
+        return token
+    
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 router = DefaultRouter()
 router.register(f'profile', AuthViewSet, basename='profile')
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
 ]
