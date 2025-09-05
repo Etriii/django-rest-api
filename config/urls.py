aa1949d9c3c360955dfb4b83714b9e70cb66d3bb
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-09-05 22:46:26 +0800

    Added documentations url

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+# FOR DOCUMENTATION :>
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")), 
+    #Documentation :>
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
+
+"""
+Swagger: http://127.0.0.1:8000/api/docs/swagger/
+Redoc: http://127.0.0.1:8000/api/docs/redoc/
+"""
+
+
